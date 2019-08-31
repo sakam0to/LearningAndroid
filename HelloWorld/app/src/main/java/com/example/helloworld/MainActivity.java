9d273b1602282d3c68bc3a3aa02812835e8b8a43
sakam0to <jingjiezhu@hotmail.com>
2019-08-31 15:56:47 -0400

    2019/08/31 first commit

--- HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.java
+++ HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.helloworld;
 
 import android.app.Activity;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends Activity {
