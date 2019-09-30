dabedfdde55ee18fb028a5a3ec7a934cc631b548
sakam0to <jingjiezhu@hotmail.com>
2019-09-30 19:44:39 -0400

    2019-09-30 Fragments2

--- Fragments/app/src/main/java/com/zhujingjie/fragments/MainActivity.java
+++ Fragments/app/src/main/java/com/zhujingjie/fragments/MainActivity.java
@@ -1,8 +1,12 @@
 package com.zhujingjie.fragments;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.Display;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        //-- get the current display info --
+        WindowManager wm = getWindowManager();
+        Display display = wm.getDefaultDisplay();
+
+        if(display.getWidth() > display.getHeight()){ //landscape mode
+            Fragment1 fragment1 = new Fragment1();
+            fragmentTransaction.replace(android.R.id.content, fragment1);//android.R.id.content refers to the current activity's content view
+        }else{//portrait mode
+            Fragment2 fragment2 = new Fragment2();
+            fragmentTransaction.replace(android.R.id.content, fragment2);
+        }
+        fragmentTransaction.commit();
     }
 }
